5c4ccfe85b4d626cc4de49d0aea39120eb221275
elzzup <guild0105@gmail.com>
2015-12-14 19:32:46 +0900

    Fix 予期しない json parameter も許容する

--- app/src/main/java/jp/ac/dendai/im/cps/footopic/bean/ImageBean.java
+++ app/src/main/java/jp/ac/dendai/im/cps/footopic/bean/ImageBean.java
@@ -1,5 +1,7 @@
 package jp.ac.dendai.im.cps.footopic.bean;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jp.ac.dendai.im.cps.footopic.R;
 import jp.ac.dendai.im.cps.footopic.util.App;
 
@@ -7,6 +9,7 @@ import jp.ac.dendai.im.cps.footopic.util.App;
  * Created by naoya on 15/12/12.
  * サムネイルの画像のURLの構造
  */
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class ImageBean {
     private String url;
     private String thumb_url;
